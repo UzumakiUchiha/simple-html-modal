3fc648e390794207188d939e00451be2efb859dc
Rohan Gaikwad <gaikwadvrohan@gmail.com>
2022-10-02 17:27:18 +0530

    on close

--- dist/simple-html-modal.js
+++ dist/simple-html-modal.js
@@ -3,6 +3,7 @@ window.simple_dialogs_counter = 0;
 const popupV3 = options => {
     const settings = Object.assign({
         type: "success",
+        id: "shm_" + new Date().getTime(),
         timeout: -1,
         title: "",
         backdrop: true,
@@ -11,21 +12,22 @@ const popupV3 = options => {
         actions: [],
         modalOpenClass: "simple-modal-open"
     }, options);
-    const dialog_html = '<div class="dialog-container">' + '<div class="dialog-content">' + '<div class="dialog-header">' + '<h5 class="dialog-title"></h5>' + '<button type="button" class="close" data-dismiss="dialog" aria-label="Close">&times;</button>' + "</div>" + '<div class="dialog-body"></div>' + '<div class="dialog-footer"></div>' + "</div>" + "</div>";
+    const dialog_html = '<div class="dialog-container">' + '<div class="dialog-content">' + '<div class="dialog-header">' + '<h5 class="dialog-title"></h5>' + '<button class="close" aria-label="Close">&times;</button>' + "</div>" + '<div class="dialog-body"></div>' + '<div class="dialog-footer"></div>' + "</div>" + "</div>";
     const dialog = window.dialog = document.createElement("dialog");
-    dialog.innerHTML = dialog_html;
+    dialog.id = settings.id;
     dialog.classList.add(`dialog-${settings.type}`);
+    dialog.innerHTML = dialog_html;
     dialog.querySelector(".dialog-header").classList.add(`dialog-${settings.type}`);
     dialog.querySelector(".dialog-body").innerHTML = settings.content;
-    const closeDialog = () => {
-        dialog.close();
+    const closeDialog = () => dialog.close();
+    dialog.querySelector(".dialog-header .close").addEventListener("click", closeDialog);
+    dialog.addEventListener("close", () => {
         dialog.parentElement.removeChild(dialog);
-        window.simple_dialogs_counter--;
+        if (settings.backdrop) window.simple_dialogs_counter--;
         if (window.simple_dialogs_counter < 1) {
             document.body.classList.remove(settings.modalOpenClass);
         }
-    };
-    dialog.querySelector(".dialog-header .close").addEventListener("click", closeDialog);
+    });
     settings.actions.forEach((action, i) => {
         let btn = document.createElement("button");
         btn.innerHTML = action.text;
@@ -46,4 +48,5 @@ const popupV3 = options => {
         dialog.show();
     }
     if (settings.timeout > 0) closeDialog();
+    return dialog;
 };
--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 window.simple_dialogs_counter = 0;
 const popupV3 = (options) => {
-    const settings = Object.assign({type: "success", timeout: -1, title: "", backdrop: true, content: "", classes: "", actions: [], modalOpenClass: "simple-modal-open"}, options);
+    const settings = Object.assign({type: "success", id: "shm_" + new Date().getTime(), timeout: -1, title: "", backdrop: true, content: "", classes: "", actions: [], modalOpenClass: "simple-modal-open"}, options);
 
+    // could have used template literal but it doesn't minify to a single line using uglify-js
     const dialog_html = '<div class="dialog-container">' +
         '<div class="dialog-content">' +
             '<div class="dialog-header">' +
                 '<h5 class="dialog-title"></h5>' +
-                '<button type="button" class="close" data-dismiss="dialog" aria-label="Close">&times;</button>' +
+                '<button class="close" aria-label="Close">&times;</button>' +
             '</div>' +
             '<div class="dialog-body"></div>' +
             '<div class="dialog-footer"></div>' +
@@ -14,21 +15,22 @@ const popupV3 = (options) => {
     '</div>';
 
     const dialog = window.dialog = document.createElement("dialog");
-    dialog.innerHTML = dialog_html;
+    dialog.id = settings.id;
     dialog.classList.add(`dialog-${settings.type}`);
+    dialog.innerHTML = dialog_html;
     dialog.querySelector('.dialog-header').classList.add(`dialog-${settings.type}`);
     dialog.querySelector('.dialog-body').innerHTML = settings.content;
 
-    const closeDialog = () => {
-        dialog.close();
+    const closeDialog = () => dialog.close();
+
+    dialog.querySelector('.dialog-header .close').addEventListener('click', closeDialog);
+    dialog.addEventListener('close', () => {
         dialog.parentElement.removeChild(dialog);
-        window.simple_dialogs_counter--;
+        if (settings.backdrop) window.simple_dialogs_counter--;
         if(window.simple_dialogs_counter < 1) {
             document.body.classList.remove(settings.modalOpenClass);
         }
-    }
-
-    dialog.querySelector('.dialog-header .close').addEventListener('click', closeDialog);
+    });
 
     settings.actions.forEach((action,i) => {        
         let btn = document.createElement("button");
@@ -54,4 +56,6 @@ const popupV3 = (options) => {
     }
 
     if(settings.timeout > 0) closeDialog();
+
+    return dialog;
 }
